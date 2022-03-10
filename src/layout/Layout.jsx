aa1a1ec47797e6f5ccd5b1bf3b84b17759114cc1
TrueVale <srvleh@gmail.com>
2022-03-10 18:25:43 +0100

    Simple layout design

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,9 +2,25 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div>
-      <h1>Layout</h1>
-      <Outlet/>
+    <div className='md:flex md:min-h-screen'>
+      <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
+        <h2 className='text-4xl font-black text-center text-white'>Client Manager</h2>
+        <nav className='mt-10'>
+          <a
+            className='text-white text-2xl block mt-2 hover:text-blue-300' 
+            href="/clients">
+            Clients
+          </a>
+          <a
+            className='text-white text-2xl block mt-2 hover:text-blue-300'
+            href="/clients/new">
+            New Client
+          </a>
+        </nav>
+      </div>
+      <div className='md:w-3/4'>
+        <Outlet/>
+      </div>
     </div>
   );
 }
