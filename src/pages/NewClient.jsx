7c2e7f597a926e572216fa4dee664802ba272a5e
TrueVale <srvleh@gmail.com>
2022-03-16 16:36:12 +0100

    Creating form component

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Form from '../components/Form';
 
 const NewClient = () => {
   return (
-    <div>
-      <h1>New Client</h1>
-    </div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>New Client</h1>
+      <p className='mt-3'>Fill next fields to sign up a new client</p>
+      <Form/>
+    </>
   );
 }
 
