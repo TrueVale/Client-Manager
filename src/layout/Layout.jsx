bd6087d71a748c75bea280c3a00ed1ad3c2af91d
TrueVale <srvleh@gmail.com>
2022-03-16 16:29:55 +0100

    Using useLocation hook

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,21 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 const Layout = () => {
+
+  const location = useLocation()
+
+  const currentUrl = location.pathname
+
   return (
     <div className='md:flex md:min-h-screen'>
       <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
         <h2 className='text-4xl font-black text-center text-white'>Client Manager</h2>
         <nav className='mt-10'>
-          <a
-            className='text-white text-2xl block mt-2 hover:text-blue-300' 
-            href="/clients">
+          <Link
+            className={`${currentUrl === '/clients' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`} 
+            to="/clients">
             Clients
-          </a>
-          <a
-            className='text-white text-2xl block mt-2 hover:text-blue-300'
-            href="/clients/new">
+          </Link>
+          <Link
+            className={`${currentUrl === '/clients/new' ? 'text-blue-300': 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+            to="/clients/new">
             New Client
-          </a>
+          </Link>
         </nav>
       </div>
       <div className='md:w-3/4'>
