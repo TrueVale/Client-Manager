2bfc1b7afd16ef9e39dd3ff86e99dd02b8f19ebd
TrueVale <srvleh@gmail.com>
2022-03-24 17:52:52 +0100

    Showing client data

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,9 +1,28 @@
 const Client = ({ client }) => {
-  return (
-    <div>
-      <h1>{ client.name }</h1>
-    </div>
-  );
+
+    const { name, company, email, phone, info, id } = client
+
+    return (
+        <tr className="border-b hover:bg-gray-100">
+            <td className="p-3">{ name }</td>
+            <td className="p-3"> 
+                <p><span className="text-gray-800 uppercase font-bold">Email: </span>{ email }</p>
+                <p><span className="text-gray-800 uppercase font-bold">Phone: </span>{ phone }</p>
+            </td>
+            <td className="p-3">{ company }</td>
+            <td className="p-3">
+                <button type="button" className="bg-green-500 hover:bg-green-600 block w-full text-white p-2 uppercase font-bold text-xs rounded-sm">
+                    More info
+                </button>
+                <button type="button" className="bg-blue-600 hover:bg-blue-700 block w-full text-white p-2 uppercase font-bold text-xs rounded-sm mt-3">
+                    Edit
+                </button>
+                <button type="button" className="bg-red-600 hover:bg-red-700 block w-full text-white p-2 uppercase font-bold text-xs rounded-sm mt-3">
+                    Delete
+                </button>
+            </td>
+        </tr>
+    );    
 }
 
 export default Client;
