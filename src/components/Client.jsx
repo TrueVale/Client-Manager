f4b4ec0bde1fed47e591c9c87b85b4431df773c1
TrueVale <srvleh@gmail.com>
2022-03-24 17:43:53 +0100

    Creating table to show clients

---
+++ src/components/Client.jsx
@@ -0,0 +1,9 @@
+const Client = ({ client }) => {
+  return (
+    <div>
+      <h1>{ client.name }</h1>
+    </div>
+  );
+}
+
+export default Client;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import Client from '../components/Client';
 
 const Home = () => {
+
+  const [ clients, setClients ] = useState([])
+
+  useEffect(() => {
+    const getClients = async () => {
+      try {
+        const url = "http://localhost:4000/clients"
+        const resp = await fetch(url)
+        const result = await resp.json()
+
+        setClients(result)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getClients()
+  }, [])
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Clients</h1>
+      <p className='mt-3'>Manage clients</p>
+      <table className='w-full mt-5 table-auto shadow bg-white'>
+        <thead className='bg-blue-800 text-white'>
+          <tr>
+            <th className='p-2'>Name</th>
+            <th className='p-2'>Contact</th>
+            <th className='p-2'>Company</th>
+            <th className='p-2'>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map( client =>(
+            <Client
+              key={ client.id }
+              client={ client }
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
