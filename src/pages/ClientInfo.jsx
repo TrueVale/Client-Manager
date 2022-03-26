b963ac7897023a2a33a4c63ea4cb891cb65cca32
TrueVale <srvleh@gmail.com>
2022-03-26 15:03:07 +0100

    Added Spinner and loading issue solved

--- src/pages/ClientInfo.jsx
+++ src/pages/ClientInfo.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const ClientInfo = () => {
 
     const [ client, setClient ] = useState({})
+    const [ loading, setLoading ] = useState(true) 
 
     const { id } = useParams()
 
@@ -17,52 +19,61 @@ const ClientInfo = () => {
             } catch (error) {
                 console.log(error)
             }
+            setLoading(!loading)
         }
         getClient()
     }, [])
 
     return (
-        <div>
 
-            <h1 className='font-black text-4xl text-blue-900'>More info about { client.name }</h1>
+        loading ? <Spinner/> : 
+        Object.keys(client).length === 0 ? 
+        <p>Client with id { id } does not exist</p> : 
+        (
 
-            { client.name && (
-                <p className="text-4xl text-gray-500 mt-10">
-                    <span className="text-gray-600 uppercase font-bold">Client:</span>
-                    { client.name }
-                </p>
-            )}
-           
-            { client.email && (
-                 <p className="text-2xl text-gray-500 mt-4">
-                    <span className="text-gray-600 uppercase font-bold">Email:</span>
-                    { client.email }
-                 </p>
+            <div>
+        
+                <h1 className='font-black text-4xl text-blue-900'>More info about { client.name }</h1>
 
-            )}
-            
-            { client.phone && (
-                <p className="text-2xl text-gray-500 mt-4">
-                    <span className="text-gray-600 uppercase font-bold">Phone:</span>
-                    { client.phone }
-                </p>
-            )}
+                { client.name && (
+                    <p className="text-4xl text-gray-500 mt-10">
+                        <span className="text-gray-600 uppercase font-bold">Client:</span>
+                        { client.name }
+                    </p>
+                )}
 
-            { client.company && (
-                <p className="text-2xl text-gray-500 mt-4">
-                    <span className="text-gray-600 uppercase font-bold">Company:</span>
-                    { client.company }
-                </p>
-            )}
+                { client.email && (
+                    <p className="text-2xl text-gray-500 mt-4">
+                        <span className="text-gray-600 uppercase font-bold">Email:</span>
+                        { client.email }
+                    </p>
 
-            { client.info && (
-                <p className="text-2xl text-gray-500 mt-4">
-                    <span className="text-gray-600 uppercase font-bold">More info:</span>
-                    { client.info }
-                </p>
-            )}
-            
-        </div>
+                )}
+
+                { client.phone && (
+                    <p className="text-2xl text-gray-500 mt-4">
+                        <span className="text-gray-600 uppercase font-bold">Phone:</span>
+                        { client.phone }
+                    </p>
+                )}
+
+                { client.company && (
+                    <p className="text-2xl text-gray-500 mt-4">
+                        <span className="text-gray-600 uppercase font-bold">Company:</span>
+                        { client.company }
+                    </p>
+                )}
+
+                { client.info && (
+                    <p className="text-2xl text-gray-500 mt-4">
+                        <span className="text-gray-600 uppercase font-bold">More info:</span>
+                        { client.info }
+                    </p>
+                )}
+                
+            </div>
+
+        )
     );
 }
 
